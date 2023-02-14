fe1a61c98b362ac98ed237bfb6aea44f58f05de5
Agimir <william@mesaindigostudios.com>
2023-02-14 12:04:21 -0600

    Added ammo images
    oh boi, that shit sharp

--- data/server_add.lua
+++ data/server_add.lua
@@ -2,9 +2,10 @@
                                         <! USABLE ITEMS !>
                     Add to ox_inventory/modules/items/server.lua
                     Add this in the items containers list for it to work and
-                    make sure you have the 'wallet' item in your items file
+                    make sure you have the item in your items file
                     (the one that came with this resource)
 -------------------------------------------------------------------------------- ]]--
+addToList {
 
   ['wallet'] = {
 		size = {5, 3000},
@@ -21,3 +22,5 @@
   ['giftbox_white'] = {
 		size = {3, 5000},
 	},
+
+}
